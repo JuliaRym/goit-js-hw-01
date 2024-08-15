c886741d487ccb86318ef97180be8df113c25fe3
JuliaRym <julka.rymanowska@gmail.com>
2024-08-15 21:49:06 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -6,10 +6,6 @@ function getElementWidth(content, padding, border) {
   return Number(a) + 2 * Number(b) + 2 * Number(c);
 }
 
-// getElementWidth("50px", "8px", "4px");
-// getElementWidth("60px", "12px", "8.5px");
-// getElementWidth("200px", "0px", "0px");
-
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
